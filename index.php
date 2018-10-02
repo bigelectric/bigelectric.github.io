429c9404c782bba330b2de7c410789bbdf96ab9f
DaShaun Carter <dashaun@dashaun.com>
2018-10-02 16:26:28 -0500

    copy

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+ 
+<meta charset="utf-8">
+<title>Big Electric - Kansas City Electrical - Liscensed and Bonded</title>
+<meta name="revisit-after" content="5"/>
+<meta name="description" content="Big Electric of Kansas City services residential home buyers and builders, specializing in Service upgrades, remodels, light fixture installation, and safety inspections"/>
+<meta name="keywords" content="kansas city, electrical, electrician, electric service upgrades, remodels, light fixture installation, safety inspections"/>
+<meta name="rating" content="Safe For Kids"/>
+<meta name="robots" content="index,folow"/>
+<meta name="author" content="Design Expressions Kansas City Web Design"/>
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+ 
+<link rel="stylesheet" href="stylesheets/base.css">
+<link rel="stylesheet" href="stylesheets/skeleton.css">
+<link rel="stylesheet" href="stylesheets/layout.css">
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+ 
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-13298265-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</head>
+<body>
+<div class="container">
+ 
+<div id="shaddow" style="height:187px;">
+<div class="four columns" id="logo">
+<img src="images/big-electric.png" width="172" height="190" alt="Big Electric">
+<h1>Kansas City - Electrical - Liscenced and Bonded - Big Electric</h1>
+</div>
+<div class="twelveh columns" id="header">
+<h2 class="remove-bottom"><span style="font-size:28px; font-weight:bold">THINK BIG</span></h2>
+<h2 class="remove-bottom" style="margin-top:-15px;">FOR ALL YOUR ELECTRICAL NEEDS</span></h2>
+<h3 class="remove-bottom"><a href="services.php">INSTALL</a></h3> <div style="float:left"><img src="images/lightning-bolt.png" alt="" width="20" height="25"></div> <h3><a href="services.php">SERVICE</a></h3> <div style="float:left"><img src="images/lightning-bolt.png" alt="" width="20" height="25"></div> <h3><a href="services.php">REPAIR</a></h3><br>
+<div id="calltoaction">Same Day Service, 7 Days A Week<br>
+Call Or Schedule Online<br>
+<span class="calltoactionphone">Kansas: 913.262.1062 &nbsp;&nbsp;&nbsp;Missouri: 816.456.5949</span>
+</div>
+</div>
+</div>
+ 
+<nav>
+<ul>
+<li><a href="index.php">HOME</a></li>
+<li><a href="about.php">ABOUT</a></li>
+<li><a href="services.php">SERVICES</a></li>
+<li><a href="portfolio.php">PORTFOLIO</a></li>
+<li><a href="testimonials.php">TESTIMONIALS</a></li>
+<li><a href="contact.php">CONTACT US</a></li>
+</ul>
+</nav>
+ 
+<div id="shaddowg" style="height:140px;">
+<div>
+<div id="appointment">
+<h4><a href="/cdn-cgi/l/email-protection#3054514655704458595e5b525957555c5553444259531e535f5d">Make an Appointment</a></h4>
+Kansas: 913.262.1062 &nbsp;&nbsp;&nbsp;<br>
+Missouri: 816.456.5949<br>
+<a href="/cdn-cgi/l/email-protection#a8ccc9decde8dcc0c1c6c3cac1cfcdc4cdcbdcdac1cb86cbc7c5"><br>
+Complete our online<br>
+Appointment Request form</a></div>
+</div>
+<div>
+<div id="service">
+<h4><a href="#" onclick="window.open('coupon.html', 'WindowA', 'width=550,height=280,scrollbars=no')">Save 10% on Upgrade!</a></h4>
+<p>Print this coupon and receive 10% off any electrical service upgrade. </p>
+<p><a href="#" onclick="window.open('coupon.html', 'WindowA', 'width=550,height=280,scrollbars=no')">Click here to save today!</a></p>
+</div>
+</div>
+<div style="float:left; margin-top:60px; margin-left:20px;">
+<img src="images/better-business-bureau.png" width="64" height="63" alt="Better Business Bureau">
+</div>
+<div style="float:right; margin-top:-11px; margin-right:-20px;"><h4>
+<div style="display:block; float:right;">
+<img src="images/dave-broderick.png" width="120" height="153" alt="Dave Broderick - Owner of Big Electric"></div><div style="margin-top:95px; float:right"><img src="images/lighbulb.png" width="39" height="50" alt="Light Bulb"></div><div style="display:block;float:right; margin-top:95px; margin-right:5px; text-align:right;"><a href="about.php" style="color:#000000; text-decoration:none;">Meet<br>
+Dave Broderick</a></div></h4></div>
+</div>
+ 
+<div style="height:400px;	margin-top: 15px; padding-top:20px;" id="shaddowb">
+<div class="one-third column" style="border-right: solid 1px #ccc; padding-right:15px;">
+<span class="bluetitle">ABOUT</span><br>
+<span class="greytitle">OUR COMPANY</span>
+<p style="padding-top:20px;"><img src="images/about.gif" width="233" height="148" alt="About Big Electric of Kansas City"><br>
+Big Electric is a Kansas City-based licensed and bonded residential service specialist offering over 30 years&rsquo; combined experience in the electrical industry.</p>
+<p><a href="about.php">Learn More</a></p>
+</div>
+<div class="one-third column" style="border-right: solid 1px #ccc; padding-right:15px;">
+<span class="bluetitle">SERVICES</span><br>
+<span class="greytitle">WE OFFER</span>
+<p style="padding-top:20px;"><img src="images/services.gif" width="233" height="148" alt="Big Electric of Kansas City Services"><br>
+Big Electric primarily services residential home buyers and builders, specializing in:</p>
+<p> <strong>· Service upgrades</strong><br>
+<strong>· Remodels</strong><br>
+<strong>· Light fixture installation</strong><br>
+<strong>· Safety inspections</strong>.</p>
+<p><a href="services.php">Learn More</a></p>
+</div>
+<div class="one-third column">
+<span class="bluetitle">CLIENT</span><br>
+<span class="greytitle">TESTIMONIALS</span>
+<p style="padding-top:20px;">"I've worked with Dave for 5 years and his professionalism and sophistication has been a constant. </p>
+<p>We&rsquo;ve worked with many electricians and contractors over the years, but Dave goes beyond his job as an electrician and installer; he functions as an expert in the trade.  </p>
+<p>That is what makes him such a great partner to our firm - our clients truly enjoy having him around.</p>
+<p>Dave is not just another trade contractor, he has become a friend and a trusted advisor. Thanks Dave!"</p>
+<p>Clay Lyon<br>
+<strong>Lyon Construction + Design, LLC</strong></p>
+</div>
+</div>
+<footer style="text-align:center; color:#666; margin-top:15px;">
+<a href="http://www.designexpressions.co" target="_blank"><img src="images/design-expressions-website-design.png" alt="Design Expressions Kansas City Website and Graphic Design Services" width="115" height="32" border="0" align="right"/></a>&copy; 2007-2016. All Rights Reserved. Big Electric, Inc.
+</footer>
+</div> 
+ 
+<script type="text/javascript">/* <![CDATA[ */(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */</script></body>
+</html>
---
+++ services.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+ 
+<meta charset="utf-8">
+<title>Big Electric - Kansas City Electrical - Liscensed and Bonded</title>
+<meta name="revisit-after" content="5"/>
+<meta name="description" content="Big Electric of Kansas City services residential home buyers and builders, specializing in Service upgrades, remodels, light fixture installation, and safety inspections"/>
+<meta name="keywords" content="kansas city, electrical, electrician, electric service upgrades, remodels, light fixture installation, safety inspections"/>
+<meta name="rating" content="Safe For Kids"/>
+<meta name="robots" content="index,folow"/>
+<meta name="author" content="Design Expressions Kansas City Web Design"/>
+ 
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+ 
+<link rel="stylesheet" href="stylesheets/base.css">
+<link rel="stylesheet" href="stylesheets/skeleton.css">
+<link rel="stylesheet" href="stylesheets/layout.css">
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+ 
+<link rel="shortcut icon" href="images/favicon.ico">
+<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-13298265-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</head>
+<body>
+<div class="container">
+ 
+<div id="shaddow" style="height:187px;">
+<div class="four columns" id="logo">
+<img src="images/big-electric.png" width="172" height="190" alt="Big Electric">
+<h1>Kansas City - Electrical - Liscenced and Bonded - Big Electric</h1>
+</div>
+<div class="twelveh columns" id="header">
+<h2 class="remove-bottom"><span style="font-size:28px; font-weight:bold">THINK BIG</span></h2>
+<h2 class="remove-bottom" style="margin-top:-15px;">FOR ALL YOUR ELECTRICAL NEEDS</span></h2>
+<h3 class="remove-bottom"><a href="services.php">INSTALL</a></h3> <div style="float:left"><img src="images/lightning-bolt.png" alt="" width="20" height="25"></div> <h3><a href="services.php">SERVICE</a></h3> <div style="float:left"><img src="images/lightning-bolt.png" alt="" width="20" height="25"></div> <h3><a href="services.php">REPAIR</a></h3><br>
+<div id="calltoaction">Same Day Service, 7 Days A Week<br>
+Call Or Schedule Online<br>
+<span class="calltoactionphone">Kansas: 913.262.1062 &nbsp;&nbsp;&nbsp;Missouri: 816.456.5949</span>
+</div>
+</div>
+</div>
+ 
+<nav>
+<ul>
+<li><a href="index.php">HOME</a></li>
+<li><a href="about.php">ABOUT</a></li>
+<li><a href="services.php">SERVICES</a></li>
+<li><a href="portfolio.php">PORTFOLIO</a></li>
+<li><a href="testimonials.php">TESTIMONIALS</a></li>
+<li><a href="contact.php">CONTACT US</a></li>
+</ul>
+</nav>
+ 
+<div class="columnse" style="margin-top: 15px; padding:15px;" id="shaddowb">
+<h1>BIG ELECTRIC <span class="grey">SERVICES</span></h1>
+<p>We are proud to be a dynamic, growing company with more than 10 years of professional electrical contracting experience. We would like Big Electric to be your first choice for quality electrical services, which is why all of our work is performed by experienced master electricians and trained employees. Your satisfaction is our top priority, and we always pay special attention to the details of each customer's request. No matter what the size of your project, Big Electric will work with you closely to provide a quality project that is completed on time and within your budget.</p>
+<ul>
+<li>Remodels &amp; New Construction</li>
+<li>Safety Inspections/Code Corrections</li>
+<li>Kitchen, Bath, Basement Finishes, New Additions</li>
+<li>Lighting Design &amp; Planning</li>
+<li>Light Fixture Installation</li>
+<li>Ceiling Fan Installation &amp; Maintenance</li>
+<li>Overhead and Underground Service</li>
+<li>Dimming Systems</li>
+<li>Telephone, Cable, Computer and Stereo Wiring</li>
+<li>Electrical Floor Heating</li>
+<li>Conventional Electric Baseboard Heating</li>
+<li>Roof and Gutter Heating Cable</li>
+<li>Back-Up Generator Systems</li>
+<li>House Surge Protection</li>
+<li>Service Panel Upgrades</li>
+<li>Plugs, Wall Outlets and Switches</li>
+<li>Breakers, Fuses</li>
+<li>Outdoor Lighting, Recessed Lighting, Track &amp; Accent Lighting, Flourescent Ballast, Exhaust Fans</li>
+<li>Landscape Lighting</li>
+<li>Hot Tubs &amp; Spas, Appliance Circuits, Electric Water Heaters, Smoke Detectors, Fire Alarms, Motors, Dryer and Range Circuits</li>
+</ul>
+<div class="servicesback" align="center">
+Every year devastating fires are the number one cause property loss. Many of the fires could have been avoided had there been a simple electrical upgrade. To prevent any unnecessary electrical fires in your home, consider hiring Big Electric to review your current electrical rewiring!</div>
+<h4><strong>OUR SERVICE GUARANTEE</strong></h4>
+<p><strong><span class="title"><img src="images/better-business-bureau.jpg" alt="Member of the Better Business Bureau" width="81" height="81" hspace="10" align="right"/></span></strong>Big Electric understands your concerns regarding costs and schedules and it's our job to help you achieve your bottom-line goals. We listen carefully and will deliver results that exceed your expectations.</p>
+<p>All of our electrical work comes with a 100% service guarantee. We use top quality, name-brand products. Our parts come with a manufacturer&rsquo;s warranty.</p>
+<h4><strong>PAYMENT OPTIONS</strong></h4>
+<p>Think Big Electric accepts cash, MasterCard and Visa.</p>
+</div>
+<footer style="text-align:center; color:#666; margin-top:15px;">
+<a href="http://www.designexpressions.co" target="_blank"><img src="images/design-expressions-website-design.png" alt="Design Expressions Kansas City Website and Graphic Design Services" width="115" height="32" border="0" align="right"/></a>&copy; 2007-2016. All Rights Reserved. Big Electric, Inc.
+</footer>
+</div>
+</div> 
+ 
+</body>
+</html>
